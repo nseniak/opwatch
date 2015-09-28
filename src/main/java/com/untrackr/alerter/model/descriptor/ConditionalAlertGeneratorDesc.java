4fff2e228db51c9b803a426bdea06175e0939ac3
nseniak <nseniak@gmail.com>
2015-09-28 15:15:00 +0200

    Removed useless fields

--- src/main/java/com/untrackr/alerter/model/descriptor/ConditionalAlertGeneratorDesc.java
+++ src/main/java/com/untrackr/alerter/model/descriptor/ConditionalAlertGeneratorDesc.java
@@ -2,29 +2,4 @@ package com.untrackr.alerter.model.descriptor;
 
 public class ConditionalAlertGeneratorDesc extends ActiveProcessorDesc {
 
-	/**
-	 * Priority (optional). Defaults to NORMAL.
-	 */
-	private String priority;
-	/**
-	 * Title. Defaults to NORMAL.
-	 */
-	private String title;
-
-	public String getPriority() {
-		return priority;
-	}
-
-	public void setPriority(String priority) {
-		this.priority = priority;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 }
