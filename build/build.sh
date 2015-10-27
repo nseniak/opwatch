f9b4d080a77d208892d9d0babceff7d3a788f2d4
Antoine Melki <antoine@ccgenie.net>
2015-10-27 23:12:58 +0100

    Now pushes backups and builds to either trackbuster-dev or trackbuster-prod buckets 1.55 version

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,11 @@ version="1.0-SNAPSHOT"
 src_jar_name="alerter-${version}.jar"
 src_jar_file=../target/${src_jar_name}
 target_jar_name="alerter.jar"
-gsutil cp ${src_jar_file} gs://trackbuster-alerter-builds/${target_jar_name} || exit 1
+environment=$1
+if [ -z "${UNTRACKR_ENVIRONMENT_NAME}" ]
+then
+  echo "Undefined variable: UNTRACKR_ENVIRONMENT_NAME" 1>&2
+  exit 1
+fi
+gsutil cp ${src_jar_file} gs://trackbuster-${env_name}/builds/${target_jar_name} || exit 1
 echo "Jar file ${src_jar_name} successfully uploaded as ${target_jar_name}"
