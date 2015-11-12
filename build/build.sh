eaf5255a2f7346a2cf2c6fe4c5e4cbeac12373ce
nseniak <nseniak@gmail.com>
2015-11-12 12:26:04 +0100

    Added environment name determination

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,7 @@ version="1.0-SNAPSHOT"
 src_jar_name="alerter-${version}.jar"
 src_jar_file=../target/${src_jar_name}
 target_jar_name="alerter.jar"
+export UNTRACKR_ENVIRONMENT_NAME=$(sh environment-name.sh $(hostname))
 if [ -z "${UNTRACKR_ENVIRONMENT_NAME}" ]
 then
   echo "Undefined variable: UNTRACKR_ENVIRONMENT_NAME" 1>&2
