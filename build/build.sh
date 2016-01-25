37071b427df94d5a2ab8c155975e30b8aef89c0d
Antoine Melki <antoine@ccgenie.net>
2016-01-25 13:42:42 +0100

    Upload alerter.jar to all known buckets

--- build/build.sh
+++ build/build.sh
@@ -3,11 +3,8 @@ version="1.0-SNAPSHOT"
 src_jar_name="alerter-${version}.jar"
 src_jar_file=../target/${src_jar_name}
 target_jar_name="alerter.jar"
-export UNTRACKR_ENVIRONMENT_NAME=$(sh environment-name.sh $(hostname))
-if [ -z "${UNTRACKR_ENVIRONMENT_NAME}" ]
-then
-  echo "Undefined variable: UNTRACKR_ENVIRONMENT_NAME" 1>&2
-  exit 1
-fi
-gsutil cp ${src_jar_file} gs://trackbuster-${UNTRACKR_ENVIRONMENT_NAME}/builds/${target_jar_name} || exit 1
+
+gsutil cp ${src_jar_file} gs://trackbuster-prod/builds/${target_jar_name} || exit 1
+gsutil cp ${src_jar_file} gs://trackbuster-dev/builds/${target_jar_name} || exit 1
+gsutil cp ${src_jar_file} gs://senders-deployment/prod/builds/${target_jar_name} || exit 1
 echo "Jar file ${src_jar_name} successfully uploaded as ${target_jar_name}"
